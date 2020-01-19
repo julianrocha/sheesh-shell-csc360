9e8a8df60da81ff72eb40dd49e77190b39c1769e
Julian Rocha <jules.r.rocha@gmail.com>
2020-01-19 13:49:12 -0800

    Created hello world project in CLion

---
+++ cmake-build-debug/CMakeFiles/sheesh.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sheesh.dir/sheesh.c.o"
+  "sheesh"
+  "sheesh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sheesh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sheesh.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    int x = 100;
+    printf("Hello, World again!");
+    return 0;
+}
